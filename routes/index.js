570db1781418bdef1a1db18dac66f6b35d5e79ce
Brodie Kime <brodie.kime@gmail.com>
2023-02-02 10:31:50 -0500

    done Brodie Kime

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+const PunkAPIWrapper = require('punkapi-javascript-wrapper')
+const punkAPI = new PunkAPIWrapper()
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index.hbs', { title: 'Home' });
 });
+
+/* GET beer page. */
+router.get('/beers', ((req, res, next)=>{
+  
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+     res.render("beers.hbs",{beersFromApi})
+  })
+  .catch(error => console.log(error));
+
+}))
+
+router.get('/random-beer', ((req, res, next)=>{
+  
+  punkAPI
+  .getRandom()
+  .then(beersFromApi => {
+    res.render("random-beer.hbs",{beersFromApi})
+  })
+  .catch(error => console.log(error));
+
+}))
 
 module.exports = router;
